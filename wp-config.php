a86873526bfb620be5f8f915400639e4cfd07208
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-07-19 09:24:37 -0500

    DB Connection 2

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'turismo' );
+define( 'DB_NAME', 'u178461197_turismo' );
 
 /** Database username */
 define( 'DB_USER', 'u178461197_redesystem_db' );
