8be5edd3199dd116de1cbd330f7f24ef0e85cd80
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-07-20 13:17:25 -0500

    .env implementation

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,12 @@
 <?php
+
+// Carga la librería dotenv
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// Carga las variables de entorno desde el archivo .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 /**
  * The base configuration for WordPress
  *
@@ -18,18 +26,20 @@
  * @package WordPress
  */
 
+ //print_r($dotenv);
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u178461197_turismo' );
+define( 'DB_NAME', $_ENV["DB_NAME"] );
 
 /** Database username */
-define( 'DB_USER', 'u178461197_redesystem_db' );
+define( 'DB_USER', $_ENV["DB_USER"] );
 
 /** Database password */
-define( 'DB_PASSWORD', '@VMSgIt@YC5g' );
+define( 'DB_PASSWORD', $_ENV["DB_PASSWORD"] );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', $_ENV["DB_HOST"] );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -82,7 +92,6 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
 
 
 /* That's all, stop editing! Happy publishing. */
