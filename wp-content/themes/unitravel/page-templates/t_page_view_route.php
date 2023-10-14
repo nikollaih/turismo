93c772405b4f0805e079e8e65cb55c4d350ff2f2
Andres Felipe Ramirez <andresfe-2010@hotmail.com>
2023-10-13 20:13:23 -0500

    Ajuste de Vista Rutas
    fix(t_page_view_route): Se ajusta la vista de la ruta y se cambia el diseño de las actividades en forma de tabla
    fix(css/pages/styles): Se ajusta los estilos de las actividades

--- wp-content/themes/unitravel/page-templates/t_page_view_route.php
+++ wp-content/themes/unitravel/page-templates/t_page_view_route.php
@@ -14,6 +14,9 @@ $activitysModel = new CUS_Activities_Route();
 $route = $routesModel->find(cus_decrypt($_GET['ruta']));
 $activitys = $activitysModel->get_all_by_route($route['id_route']);
 ?>
+<style>
+
+</style>
 <div id="custom-page">
     <?php get_header() ?>
     <div id="primary" class="site-content">
@@ -23,7 +26,7 @@ $activitys = $activitysModel->get_all_by_route($route['id_route']);
                         <div class="row content-profile-one mb-5">
                             <div class="col-md-12 col-sm-12 ">
                                 <div class="row">
-                                    <div class="col-md-6 col-xs-12">
+                                    <div class="col-md-6  col-xs-12">
                                         <img src="<?= get_route_logo($route["id_route"], $route["route_image"]) ?>" alt="" srcset="" class="img-fluid img-same-height">
                                     </div>
                                     <div class="col-md-6 col-xs-12  ">
@@ -57,8 +60,8 @@ $activitys = $activitysModel->get_all_by_route($route['id_route']);
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6 col-xs-12">
+                        <div class="row justify-content-center">
+                            <div class="col-md-8 col-xs-12">
                                 <div class="mt-5">
                                     <h5 class="title-route sc_item_title sc_title_title sc_item_title_style_default">Descripcción</h5>
                                     <div class="route_short_description">
@@ -74,32 +77,45 @@ $activitys = $activitysModel->get_all_by_route($route['id_route']);
                                 <div class="mt-5">
                                     <h5 class="title-route sc_item_title sc_title_title sc_item_title_style_default">Actividades</h5>
                                 </div> 
-                                <?php if(!empty($activitys )) :?> 
-                                        <?php $activityCount = 1;?> 
-                                            <?php foreach($activitys as $activity) :?>
-                                            <div class="container mt-5">
-                                                <div class="d-flex">
-                                                    <div class="outer-circle">
-                                                        <div class="inner-circle">
-                                                            <span class="number"><?= $activityCount ?></span>
+                            </div>
+                            <div class="col-md-4 col-xs-12">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 ">
+                                <div class="row">
+                                    <?php if(!empty($activitys )) :?> 
+                                            <?php $activityCount = 1;?> 
+                                                <?php foreach($activitys as $activity) :?>
+                                                <div class="container mt-5">
+                                                    <div class="row">
+                                                        <div class="col-md-2 d-flex text-center justify-content-center align-items-center">
+                                                            <div class="outer-circle">
+                                                                <div class="inner-circle">
+                                                                    <span class="number"><?= $activityCount ?></span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4 d-flex justify-content-center align-items-center text-center ">
+                                                            <span class="room-title"><?= $activity['activity_name'] ?></span>
+                                                        </div>
+                                                        <div class=" col-md-3 d-flex justify-content-center align-items-center text-center">
+                                                            <p class="route_short_description"><?= !empty($activity['activity_location_other']) ? $activity['activity_location_other'] : $activity['cus_company_name'] ?></p>
+                                                        </div>
+                                                        <div class="col-md-3 d-flex justify-content-center align-items-center text-center">
+                                                            <span style="color: #eb5d11; font-size: initial; font-weight: 600;">
+                                                                <i class="fa-regular fa-clock"></i>&nbsp;&nbsp;&nbsp;<?= date("h:i a", strtotime($activity["activity_start_time"])) ?>&nbsp;&nbsp;-&nbsp;&nbsp;<?= date("h:i a", strtotime($activity["activity_end_time"])) ?>
+                                                            </span>
                                                         </div>
                                                     </div>
-                                                    <div class="col-md-6">
-                                                        <span class="room-title"><?= $activity['activity_name'] ?></span>
-                                                        <p class="route_short_description"><?= !empty($activity['activity_location_other']) ? $activity['activity_location_other'] : $activity['cus_company_name'] ?></p>
-                                                        <span style="color: #eb5d11; font-size: initial; font-weight: 600;">
-                                                            <i class="fa-regular fa-clock"></i>&nbsp;&nbsp;&nbsp;<?= date("h:i a", strtotime($activity["activity_start_time"])) ?>&nbsp;&nbsp;-&nbsp;&nbsp;<?= date("h:i a", strtotime($activity["activity_end_time"])) ?>
-                                                        </span>
-                                                    </div>
                                                 </div>
-                                            </div>
-                                            <?php $activityCount++; ?> 
-                                        <?php endforeach; ?>
-                                    <?php endif;?>
+                                                <?php $activityCount++; ?> 
+                                                <hr style="border: 1px solid #edbb99; width:100%">
+                                            <?php endforeach; ?>
+                                        <?php endif;?>
+                                </div>               
                             </div>
-                            <div class="col-md-6 col-xs-12">
-                            </div>
-                     </div>
+                        </div>
                 </div>
             </section>
         </div>
