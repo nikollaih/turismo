d3c5930dab98f1491704561571f7ee5549a5fa43
Andres Felipe Ramirez <andresfe-2010@hotmail.com>
2023-11-20 20:03:23 -0500

    Settings view company
    fix(pages/styles.css): Se ajusta los favicon y el media query del banner
    feat(company_helper): Se agrega helper para poder consultar toda la informacion de la finca en especifico
    fix(t_page_company): Se comenta video. galeria de imagenes y se agrega la ruta aliadas a ese finca

--- wp-content/themes/unitravel/includes/helpers/company_helper.php
+++ wp-content/themes/unitravel/includes/helpers/company_helper.php
@@ -8,6 +8,11 @@ function cus_get_company($user_id){
     return (count($company_data) > 0) ? $company_data[0] : false;
 }
 
+function cus_get_company_id($id_company) {
+    require_once get_template_directory().'/includes/custom-clases/CUS_Companies.php';
+    $companies = new CUS_Companies();
+    return $companies->get_company_by_param('id_cus_company',$id_company);
+}
 function cus_get_companies_all($city_id) {
     require_once get_template_directory().'/includes/custom-clases/CUS_Companies.php';
     $companies = new CUS_Companies();
